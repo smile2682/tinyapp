415844a41f06df27cd47daf555e4667e26be8b4e
smile2682 <smilecrystal.liyi@gmail.com>
2021-08-03 01:03:25 +0000

    adding .post to deal with user output

--- express_server.js
+++ express_server.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const ejs = require("ejs");
 const app = express();
+
 const PORT = 8080; // default port 8080
 
+const ejs = require("ejs");
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -31,7 +36,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
